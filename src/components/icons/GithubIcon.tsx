65c3ccddefbe7e66eec6f3a8c04d2d468329c0aa
micolka <mic_la@tut.by>
2021-01-05 00:43:01 +0300

    fix: fix routes and footer

--- src/components/icons/GithubIcon.tsx
+++ src/components/icons/GithubIcon.tsx
@@ -1,7 +1,7 @@
-import Icon from '@/assets/svg/Github.svg';
 import React from 'react';
 import type { FunctionComponent, HTMLAttributes } from 'react';
 
+import Icon from '@/assets/svg/test.svg';
 import BaseIcon from '@/components/icons/BaseIcon';
 
 const GithubIcon: FunctionComponent<HTMLAttributes<HTMLElement>> = ({ ...restProps }) => (
