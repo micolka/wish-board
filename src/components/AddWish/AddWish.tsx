e4b5cf03504b8dce14c9597395cadc7a5582fe69
Vladimir <17500175@mail.ru>
2021-01-17 16:35:05 +0300

    fix: lint remarks

--- src/components/AddWish/AddWish.tsx
+++ src/components/AddWish/AddWish.tsx
@@ -14,50 +14,58 @@ const AddWish: FunctionComponent<HTMLAttributes<HTMLDivElement>> = () => {
   const [Tag, setTag] = useState('');
   const [Visiblity, setVisiblity] = useState('');
 
-  const ChangeWishName = (e: any): void => {
+  const ChangeWishName = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setWishName(e.currentTarget.value);
   };
-  const ChangeDescription = (e: any): void => {
+  const ChangeDescription = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setDescription(e.currentTarget.value);
   };
-  const ChangeValue = (e: any): void => {
+  const ChangeValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setValue(e.currentTarget.value);
   };
-  const ChangeSite = (e: any): void => {
+  const ChangeSite = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSite(e.currentTarget.value);
   };
-  const ChangeCurrency = (e: any): void => {
+  const ChangeCurrency = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     setCurrency(e.currentTarget.value);
   };
-  const SelectFile = (e: any): void => {
-    setSelectedFile(e.target.files[0]);
-    const divElem: any = document.getElementById('preview');
+  const SelectFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.files) {
+      setSelectedFile(e.target.files[0]);
+    }
+    const divElem: HTMLElement | null = document.getElementById('preview');
     function preview(file: any) {
       const reader = new FileReader();
-      reader.addEventListener('load', (event: any) => {
-        const img: any = document.createElement('img');
-        img.style.cssText = 'position:absolute; max-width:100%; max-height:100%';
-        img.src = event.target.result;
-        divElem.appendChild(img);
+      reader.addEventListener('load', (event: ProgressEvent<FileReader>) => {
+        if (event.target) {
+          const img: HTMLImageElement = document.createElement('img');
+          img.style.cssText = 'position:absolute; max-width:100%; max-height:100%';
+          img.src = event.target.result;
+          if (divElem) {
+            divElem.appendChild(img);
+          }
+        }
       });
       reader.readAsDataURL(file);
     }
-    preview(e.target.files[0]);
-
+    if (e.target.files) {
+      preview(e.target.files[0]);
+    }
   };
 
-  const ChangeCollection = (e: any): void => {
+  const ChangeCollection = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setCollection(e.currentTarget.value);
   };
-  const ChangeTag = (e: any): void => {
+  const ChangeTag = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setTag(e.currentTarget.value);
   };
-  const ChangeVisiblity = (e: any): void => {
-    setVisiblity(e.target.value);
+  const ChangeVisiblity = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setVisiblity(e.currentTarget.value);
   };
 
-  const submitForm = (e: any): void => {
+  const submitForm = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    // eslint-disable-next-line no-console
     console.log(`WishName-${WishName}, Description-${Description}, selectedFile-${selectedFile},
     Value-${Value}, Currency-${Currency}, Site-${Site}, Collection-${Collection}, Tag-${Tag}, Visiblity-${Visiblity}`);
   };
@@ -65,24 +73,34 @@ const AddWish: FunctionComponent<HTMLAttributes<HTMLDivElement>> = () => {
   return (
     <div className={styles['addWish_wrapper']}>
       <div className={styles['addWish_content']}>
-
         <h1>Я хочу...</h1>
         <form onSubmit={submitForm}>
           <div className={styles['wish_descriptions']}>
             <div>
-              <label htmlFor='descriptions'>
+              <label htmlFor="descriptions">
                 Название *
-                <input type="text" required className={styles['wishName']} onChange={ChangeWishName} id='descriptions' />
+                <input
+                  type="text"
+                  required
+                  className={styles['wishName']}
+                  onChange={ChangeWishName}
+                  id="descriptions"
+                />
               </label>
             </div>
             <div className={styles['desriptionOfWish']}>
-              <label htmlFor='description'>
+              <label htmlFor="description">
                 Описание
-                <textarea name="description" id="description" rows={8} onChange={ChangeDescription} />
+                <textarea
+                  name="description"
+                  id="description"
+                  rows={8}
+                  onChange={ChangeDescription}
+                />
               </label>
             </div>
             <div className={styles['addFile']}>
-              <label htmlFor='addFile'>
+              <label htmlFor="addFile">
                 Изображение или цветной фон
                 <input type="file" accept="image/*" onChange={SelectFile} id="upload" />
               </label>
@@ -90,13 +108,13 @@ const AddWish: FunctionComponent<HTMLAttributes<HTMLDivElement>> = () => {
             </div>
             <div className={styles['wishCost']}>
               <div className={styles['cost']}>
-                <label htmlFor='wishCost'>
+                <label htmlFor="wishCost">
                   Стоимость?
                   <input type="text" className={styles['wishPrice']} onChange={ChangeValue} />
                 </label>
               </div>
               <div className={styles['costCurrency']}>
-                <label htmlFor='costCurrency'>
+                <label htmlFor="costCurrency">
                   Валюта
                   <select value={Currency} onChange={ChangeCurrency}>
                     <option value="rub">руб</option>
@@ -107,36 +125,36 @@ const AddWish: FunctionComponent<HTMLAttributes<HTMLDivElement>> = () => {
               </div>
             </div>
             <div>
-              <label htmlFor='site'>
+              <label htmlFor="site">
                 Ссылка на сайт
                 <input type="text" onChange={ChangeSite} />
               </label>
             </div>
             <div>
-              <label htmlFor='collection'>
+              <label htmlFor="collection">
                 Добавить в колекцию
                 <input type="text" onChange={ChangeCollection} />
               </label>
             </div>
             <div>
-              <label htmlFor='tags'>
+              <label htmlFor="tags">
                 Теги
                 <input type="text" onChange={ChangeTag} />
               </label>
             </div>
-            <div className={styles['visibleProperties']} onChange={ChangeVisiblity}>
+            <div className={styles['visibleProperties']}>
               <div>
-                <input type="radio" value="All" name="visibleWish" />
+                <input type="radio" value="All" name="visibleWish" onChange={ChangeVisiblity} />
                 {' '}
                 Всем видно
               </div>
               <div>
-                <input type="radio" value="Friends" name="visibleWish" />
+                <input type="radio" value="Friends" name="visibleWish" onChange={ChangeVisiblity} />
                 {' '}
                 Друзьям
               </div>
               <div>
-                <input type="radio" value="OnlyMe" name="visibleWish" />
+                <input type="radio" value="OnlyMe" name="visibleWish" onChange={ChangeVisiblity} />
                 {' '}
                 Только мне
               </div>
